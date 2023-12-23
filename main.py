3d4e56709129efb71f2b1925df95aaf3bebdf69c
MichalDevelop <150704080+MichalDevelop@users.noreply.github.com>
2023-12-23 13:14:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from mainTherapy import
+import discord
+from discord.ext import commands
+from memegen import meme
+intents = discord.Intents.default()
+intents.message_content = True
+
+TOKEN = 'MTE4NTU0MTQ3NTc1MzI4MzU4NQ.Gd-81r.l8XKUSXZTQ6U8fnb8FNv-lTvzynSEXQDS-RP2E'
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def memegen(ctx):
+    await ctx.reply(meme())
+
+bot.run(TOKEN)
