4cdf625658a76e592a2e6bea08edfda076b9e930
Marcelusek <olbrychtmarcel@gmail.com>
2023-12-29 13:40:30 +0100

    added the therapy mode to main.py, kept the old code just incase

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from mainTherapy import
+# from mainTherapy import
+# import discord
+# from discord.ext import commands
+# from memegen import meme
+# from .mainTherapy import therapyFile
+# intents = discord.Intents.default()
+# intents.message_content = True
+
+# TOKEN = 'jaki dzban tu wsadził token?'
+
+# bot = commands.Bot(command_prefix='!', intents=intents)
+
+# @bot.command()
+# async def memegen(ctx):
+#     await ctx.reply(meme())
+
+
+# @bot.command()
+# async def therapyFile(ctx):
+#     therapyFile(ctx)
+#     await ctx.send(f'Generating response...')
+    
+
+# bot.run('TOKEN')
+
+from mainTherapy import bot, chatgpt_response
 import discord
 from discord.ext import commands
 from memegen import meme
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-TOKEN = 'MTE4NTU0MTQ3NTc1MzI4MzU4NQ.Gd-81r.l8XKUSXZTQ6U8fnb8FNv-lTvzynSEXQDS-RP2E'
+TOKEN = 'nie wsadzać tu na razie tokenu błagam was'
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -13,4 +39,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def memegen(ctx):
     await ctx.reply(meme())
 
+@bot.command()
+async def run_therapy(ctx):
+    await chatgpt_response(ctx)
+    await ctx.send(f'Generating response...')
+
 bot.run(TOKEN)
+
--- mainTherapy.py
+++ mainTherapy.py
@@ -1,30 +1,63 @@
-def therapyFile():
-    import discord
-    from discord.ext import commands    
-    import openai
-    from dotenv import load_dotenv
-    import os
+# def therapyFile():
+#     import discord
+#     from discord.ext import commands    
+#     import openai
+#     from dotenv import load_dotenv
+#     import os
 
-    load_dotenv()
+#     load_dotenv()
 
-    intents = discord.Intents.default()
-    intents.message_content = True 
-    bot = commands.Bot(command_prefix='\/', intents=intents)
+#     intents = discord.Intents.default()
+#     intents.message_content = True 
+#     bot = commands.Bot(command_prefix='\/', intents=intents)
 
-    # Set up the OpenAI API key
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+#     # Set up the OpenAI API key
+#     openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    @bot.command(name='therapy_mode')
-    async def therapy_mode(ctx):
-        therapy_mode = True
+#     @bot.command(name='therapy_mode')
+#     async def therapy_mode(ctx):
+#         therapy_mode = True
 
-    @bot.command(name='therapy_')
-    async def chatgpt_response(ctx):
-        prompt = f"User: {ctx.message.content}\n"
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=150
-        )
-        await ctx.send(response.choices[0].text.strip())
+#     @bot.command(name='therapy_')
+#     async def chatgpt_response(ctx):
+#         prompt = f"User: {ctx.message.content}\n"
+#         response = openai.Completion.create(
+#             engine="text-davinci-002",
+#             prompt=prompt,
+#             max_tokens=150
+#         )
+#         await ctx.send(response.choices[0].text.strip())
+#         return response
+
+import discord
+from discord.ext import commands    
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True 
+bot = commands.Bot(command_prefix='\/', intents=intents)
+
+# Set up the OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+@bot.command(name='therapy_mode')
+async def therapy_mode(ctx):
+    therapy_mode = True
+
+@bot.command(name='therapy_')
+async def chatgpt_response(ctx):
+    prompt = f"User: {ctx.message.content}\n"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150
+    )
+    await ctx.send(response.choices[0].text.strip())
+    return response
+
+
 
