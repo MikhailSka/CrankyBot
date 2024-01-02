c1153f90696ba9c0bd5de9958b42a2ec6db55d5c
Marcelusek <olbrychtmarcel@gmail.com>
2024-01-02 11:07:36 +0100

    tried to make mainTherapy work

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-# from mainTherapy import
-# import discord
-# from discord.ext import commands
-# from memegen import meme
-# from .mainTherapy import therapyFile
-# intents = discord.Intents.default()
-# intents.message_content = True
-
-# TOKEN = 'jaki dzban tu wsadził token?'
-
-# bot = commands.Bot(command_prefix='!', intents=intents)
-
-# @bot.command()
-# async def memegen(ctx):
-#     await ctx.reply(meme())
-
-
-# @bot.command()
-# async def therapyFile(ctx):
-#     therapyFile(ctx)
-#     await ctx.send(f'Generating response...')
-    
-
-# bot.run('TOKEN')
-
-from mainTherapy import bot, chatgpt_response
 import discord
 from discord.ext import commands
 from memegen import meme
+from mainTherapy import bot as therapy_bot, chatgpt_response
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-TOKEN = 'nie wsadzać tu na razie tokenu błagam was'
+TOKEN = 'bot_token_here'
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -41,6 +16,7 @@ async def memegen(ctx):
 
 @bot.command()
 async def run_therapy(ctx):
+    print("Command executed")
     await chatgpt_response(ctx)
     await ctx.send(f'Generating response...')
 
--- mainTherapy.py
+++ mainTherapy.py
@@ -1,63 +1,45 @@
-# def therapyFile():
-#     import discord
-#     from discord.ext import commands    
-#     import openai
-#     from dotenv import load_dotenv
-#     import os
-
-#     load_dotenv()
-
-#     intents = discord.Intents.default()
-#     intents.message_content = True 
-#     bot = commands.Bot(command_prefix='\/', intents=intents)
-
-#     # Set up the OpenAI API key
-#     openai.api_key = os.getenv('OPENAI_API_KEY')
-
-#     @bot.command(name='therapy_mode')
-#     async def therapy_mode(ctx):
-#         therapy_mode = True
-
-#     @bot.command(name='therapy_')
-#     async def chatgpt_response(ctx):
-#         prompt = f"User: {ctx.message.content}\n"
-#         response = openai.Completion.create(
-#             engine="text-davinci-002",
-#             prompt=prompt,
-#             max_tokens=150
-#         )
-#         await ctx.send(response.choices[0].text.strip())
-#         return response
-
 import discord
-from discord.ext import commands    
+from discord.ext import commands
 import openai
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True 
-bot = commands.Bot(command_prefix='\/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Set up the OpenAI API key
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
-@bot.command(name='therapy_mode')
-async def therapy_mode(ctx):
-    therapy_mode = True
+openai.api_key = 'api key here'
 
-@bot.command(name='therapy_')
 async def chatgpt_response(ctx):
     prompt = f"User: {ctx.message.content}\n"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150
-    )
-    await ctx.send(response.choices[0].text.strip())
-    return response
+    
+    while True:
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-002",
+                prompt=prompt,
+                max_tokens=150
+            )
+            break 
+        except openai.error.RateLimitError as e:
+          
+            if 'Retry-After' in e.headers:
+                wait_time = int(e.headers['Retry-After'])
+            else:
+      
+                wait_time = 10
+            
+            await asyncio.sleep(wait_time)
+
+    await ctx.send(response['choices'][0]['text'].strip())
+
+
+
+
 
 
 
